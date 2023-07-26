855fa06856baa376ec52877f656f85554c9cde40
acharyaprakash30 <acharyaprakash030@gmail.com>
2023-07-26 07:29:37 +0545

    conditional

--- 1.Datatypes/object.js
+++ 1.Datatypes/object.js
@@ -1,10 +1,23 @@
-let  student = {
-    name:"prakash",
-    address:"butwal",
-    contactNO:9867584199
-}
-// console.log(student);
-console.log(student["name"])
-console.log(student.address)
+// let  student = {
+//     firstName:"prakash",
+//     lastName:"acharya",
+//     address:"butwal",
+//     contactNO:9867584199,
+//     getFullName:function(){
+//       return this.firstName + " " + this.lastName
+//     }
+// }
+// // console.log(student);
+// // console.log(student["name"])
+// console.log(student.getFullName())
+
+
+
+
+
+
+
+
+
 
 
